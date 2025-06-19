515f5d351856c2eed1e28d82f23f853fd22a98b0
Jean Pierre Moreyra <2301010390@undc.edu.pe>
2025-06-18 23:38:17 -0500

    Horario y agregar actividades 1.0

--- app/src/main/java/com/example/studentcalendar/SplashActivity.kt
+++ app/src/main/java/com/example/studentcalendar/SplashActivity.kt
@@ -9,7 +9,7 @@ import com.bumptech.glide.Glide
 import com.example.studentcalendar.databinding.ActivitySplashBinding
 import com.google.firebase.auth.FirebaseAuth
 
-class PantallaDeCargaActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
     private lateinit var auth: FirebaseAuth
